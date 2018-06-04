62a833102db487cff202c10473b57eef54493587
Bati <bati.odon@gmail.com>
2018-06-03 22:31:07 -0500

    lab5

--- lab5.js
+++ lab5.js
@@ -1,34 +1,49 @@
 var express = require('express');
 var fetch = require("node-fetch");
+var rxjs = require("rxjs");
 var app = express();
 var port = 3000;
 var res= "";
+const {Observable } = rxjs;
 
-function getPromise () {
-    return fetch("http://jsonplaceholder.typicode.com/users/");
+var fetchContent = function(url) {
+    return Observable.create(function (observer) {
+        request(url, function (error, response, body) {
+            if (error) { observer.onError(); }
+            else { observer.onNext({response: response, body: body }); }
+            observer.onCompleted();
+        })
+    });
+};
+
+
+
+console.log(res);
+
+app.enable('case sensitive routing');
+
+
+const request = async (res , rop) => {
+    const response = await fetch("http://jsonplaceholder.typicode.com/users/");
+    const json = await response.json();
+    await rop.send(json);
+    await rop.end();
+    await console.log(json);
+    return await json;
 }
 
-async function sendReq() {
-   try {
-        let result = await getPromise();
-        return result;
-    } catch (error) {
-        return "Error";
-    }
-}
-console.log("WWWWWWWWWWW");
-console.log(sendReq().then(data=> {console.log(data)}));
-console.log("GGGGGGGGGGG");
-/*
+console.log("Promise and Async");
+
+
+
 app.get('/users' , function(res , rop){
     rop.status(200);
     rop.set('Content-Type' , 'text/html');
-    rop.send('Hi');
-    rop.end();
+    request(res , rop);
 });
 
 
 
 app.listen(port , () => {
     console.log("Server is listening");
-});*/
+});
